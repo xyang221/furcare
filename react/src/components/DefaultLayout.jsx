1845801f9303ac41a9d5556bdc7e75964b110ad8
Dianne Bastillada <diannebastillada87@gmail.com>
2023-07-28 15:19:27 +0800

    default testing

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+
+export default function DefaultLayout() {
+    const {user,token} = useStateContext()
+
+    if(!token) {
+        return <Navigate to="/login" />
+    }
+    
+    
+    return (
+        <div>
+            Default testing
+            <Outlet/>
+        
+        </div>
+    )
+}
---
+++ react/src/router.jsx
@@ -0,0 +1,49 @@
+import {Navigate, createBrowserRouter} from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Users from "./pages/Users";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<DefaultLayout />,
+        children:[
+            {
+                path: '/',
+                element:<Navigate to="/users" />
+            },
+            {
+                path:'/users',
+                element:<Users />
+            },
+            {
+                path:'/dashboard',
+                element:<Dashboard />
+            },
+        ]
+    },
+    {
+        path:'/',
+        element:<GuestLayout/>,
+        children:[
+            {
+                path:'/login',
+                element:<Login />
+            },
+            {
+                path:'/signup',
+                element:<Signup />
+            },
+        ]
+    },
+    {
+        path:'*',
+        element:<NotFound />
+    }
+])
+
+export default router;
