71bdff3738e27f32d93540f25c6f3cc28a44c772
Dianne Bastillada <diannebastillada87@gmail.com>
2023-08-05 14:12:32 +0800

    user crud sample

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,8 +1,10 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
 
 export default function DefaultLayout() {
-    const {user,token} = useStateContext()
+    const {user,token, setUser, setToken} = useStateContext()
 
     if(!token) {
         return <Navigate to="/login" />
@@ -10,7 +12,21 @@ export default function DefaultLayout() {
     
     const onLogout = (ev) => {
         ev.preventDefualt()
+
+        axiosClient.post('/logout')
+        .then(() => {
+            setUser({})
+            setToken(null)
+        })
     }
+
+    useEffect( () => {
+        axiosClient.get('/user')
+            .then(({data}) => {
+                setUser(data)
+            })
+    }, [])
+
     return (
         <div id="defaultLayout">
             <aside>
--- react/src/pages/Login.jsx
+++ react/src/pages/Login.jsx
@@ -1,9 +1,43 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom"
+import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../axios-client";
 
 export default function Login() {
+    
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    
+    const [errors,setErrors] = useState(null);
+
+    const {setUser, setToken} = useStateContext;
 
     const onSubmit = (ev) => {
         ev.preventDefault()
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+
+        setErrors(null)
+
+        axiosClient.post('/login', payload)
+            .then(({data}) => {
+                setUser(data.user)
+                setToken(data.token)
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status == 422) {
+                    if (response.data.errors) {
+                    setErrors(response.data.errors);
+                    } else {
+                        setErrors({
+                            email: [response.data.message]
+                        })
+                    }
+                }
+            })
     }
 
     return (
@@ -11,9 +45,17 @@ export default function Login() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">LOGIN</h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button className="btn btn-block">Login</button>
+                    {errors && <div className="="alert>
+                        {Object.keys(errors).map(key=> (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                        </div>
+                         
+                    }
+                    <input ref={emailRef} type="email" placeholder="Email Address" />
+                    {/* <input ref={Ref} type="username" placeholder="Username" /> */}
+                    <input ref={passwordRef} type="password" placeholder="Password" />
+                     <button className="btn btn-block">Login</button>
                     <p className="message">
                         Don’t have an account yet? <Link to="/signup">Sign Up</Link>
                     </p>
