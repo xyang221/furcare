2a07a03eb0fab21a600e909e199e43364c777c24
Dianne Bastillada <diannebastillada87@gmail.com>
2023-08-05 22:49:15 +0800

    defaultlayout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -4,37 +4,50 @@ import { useEffect } from "react";
 import axiosClient from "../axios-client";
 
 export default function DefaultLayout() {
-    const {user,token} = useStateContext()
+    const {user,token, setUser, setToken} = useStateContext()
 
-    if (!token) {
-        return <Navigate to="/login" />;
+    if(!token) {
+        return <Navigate to="/login" />
     }
-
+    
     const onLogout = (ev) => {
         ev.preventDefualt()
+
+        axiosClient.post('/logout')
+        .then(() => {
+            setUser({})
+            setToken(null)
+        })
     }
+
+    useEffect( () => {
+        axiosClient.get('/user')
+            .then(({data}) => {
+                setUser(data)
+            })
+    }, [])
+
     return (
         <div id="defaultLayout">
             <aside>
                 <Link to="/dashboard">Dashboard</Link>
-                <Link to="/register-client">Registration</Link>
                 <Link to="/users">Users</Link>
             </aside>
             <div className="content">
                 <header>
-                    <div>Header</div>
+                    <div>
+                        Header
+                    </div>
                     <div>
                         {user.name}
-                        <a href="#" onClick={onLogout} className="btn-logout">
-                            Logout
-                        </a>
+                        <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
                     </div>
                 </header>
                 <main>
-                    <Outlet />
+                <Outlet/>
                 </main>
+                
             </div>
-            {notification && <div className="notification">{notification}</div>}
         </div>
-    );
+    )
 }
