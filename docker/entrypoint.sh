ca0fda15a2dda4999cf337af82f4cd0b6724d872
Dianne Bastillada <diannebastillada87@gmail.com>
2024-04-08 23:43:36 +0800

    fixed migrate first

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,18 +2,18 @@
 
 composer install --no-progress --no-interaction
 
-if [ ! -f "/var/www/first_start.txt" ]; then
-    sleep 120
-    php artisan db:seed
-    touch /var/www/first_start.txt
-fi
-
 php artisan migrate
 php artisan key:generate
 php artisan storage:link
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
+
+if [ ! -f "/var/www/first_start.txt" ]; then
+    sleep 120
+    php artisan db:seed
+    touch /var/www/first_start.txt
+fi
 
 # Add a delay before starting the Laravel application
 sleep 120
